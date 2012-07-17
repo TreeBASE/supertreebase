4643f5a82f70121bd2db3db855056e76dc6535fe
Rutger Vos <rutgeraldo@gmail.com>
2012-07-17 04:23:13 +0200

    more elaborate usage example, with recordSchema

--- script/fetch_treebase_trees.pl
+++ script/fetch_treebase_trees.pl
@@ -7,14 +7,15 @@ use Bio::Phylo::Factory;
 use Bio::Phylo::IO 'unparse';
 
 # process command line arguments
-my ( $query, $format, $section, $help );
+my ( $query, $format, $section, $help, $recordSchema );
 GetOptions(
-	'query=s'   => \$query,
-	'format=s'  => \$format,
-	'section=s' => \$section,
-	'help|?'    => \$help,
+	'query=s'        => \$query,
+	'format=s'       => \$format,
+	'section=s'      => \$section,
+	'recordSchema=s' => \$recordSchema,
+	'help|?'         => \$help,
 );
-die "Usage: $0 --query='dcterms.identifier=S1787' --format=newick --section=study\n" if $help;
+die "Usage: $0 -q 'dcterms.title==\"Homo sapiens\"' -f newick -s taxon -r tree\n" if $help;
 
 # instantiate factory
 my $fac = Bio::Phylo::Factory->new;
@@ -27,14 +28,16 @@ my $client = $fac->create_client(
 
 # run query on client
 my $desc = $client->get_query_result(
-	'-query'   => uri_escape($query),
-	'-section' => $section,
+	'-query'        => uri_escape($query),
+	'-section'      => $section,
+	'-recordSchema' => $recordSchema,
 );
 
 # iterate over resulting resources
 for my $res ( @{ $desc->get_entities } ) {
 
 	# fetch resource data
+	$client->set_section($recordSchema);
     my $proj = $client->get_record( '-guid' => $res->get_guid );
     
     # print output
