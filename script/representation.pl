01c9f24cd0721d6e4ee7953c30ff6aca542e585c
rvosa <rutgeraldo@gmail.com>
2013-02-26 17:50:12 +0100

    now operates on taxa.txt file that includes study counts, writes representation for all NCBI taxa

--- script/representation.pl
+++ script/representation.pl
@@ -16,13 +16,15 @@ my @roots = (
 #	12908, # "unclassified"
 );
 
-# unclassified taxa to ignore
+# unclassified taxa to ignore, e.g.:
 my %block = (
 	'1130266' => 1,
 	'1154676' => 1,
 	'1154675' => 1,
 	'447827'  => 1,
 	'1297'    => 1,
+	'42452'   => 1,
+	'2323'    => 1,
 );
 
 # process command line arguments
@@ -40,11 +42,15 @@ my $log = Bio::Phylo::Util::Logger->new(
 	'-class' => 'main',
 	'-level' => $verbosity,
 );
+$log->info("connecting to flatfile DB");
+$log->info("nodes = $nodesfile");
+$log->info("names = $namesfile");
+$log->info("dir   = $directory");
 my $dbt = Bio::DB::Taxonomy->new(
 	'-source'    => 'flatfile',
-    '-nodesfile' => $nodesfile,
-    '-namesfile' => $namesfile,
-    '-directory' => $directory,	
+	'-nodesfile' => $nodesfile,
+	'-namesfile' => $namesfile,
+	'-directory' => $directory,	
 );
 $log->info("instantiated connection to NCBI flat file db");
 
@@ -53,23 +59,31 @@ my %taxon;
 {
 	$log->info("going to read from $infile");
 	open my $fh, '<', $infile or die $!;
+
+	# each line consists of occurrence and taxon ID
 	LINE: while(<$fh>) {
 		chomp;
-		if ( $block{$_} ) {
-			$log->info("ignoring unclassified taxon $_");
-			next LINE;
+		if ( /^\s*(\d+)\s+(\d+)/ ) {
+			my ( $count, $id ) = ( $1, $2 );
+			if ( $block{$id} ) {
+				$log->info("ignoring unclassified taxon $id");
+				next LINE;
+			}
+			$taxon{$id} = $count;
 		}
-		$taxon{$_} = 1;
+		else {
+			$log->warn("unexpected pattern in $infile: $!");
+		}
 	}
 	$log->info("read ".scalar(keys(%taxon))." taxon IDs");
 }
 
 # print header
-print "ID\tName\tRank\tTreeBASE\tNCBI\n";
+print "ID\tName\tRank\tStudies\tChildCount\tPhylum\tPos\n";
 $|++;
 
 # temp variables
-my ( $in_treebase, $in_ncbi, %before_ncbi, %before_treebase ) = ( 0, 0 );
+my ( $in_treebase, $pos, %before_treebase, $phylum ) = ( 0, 0 );
 
 # iterate over ncbi roots
 for my $root ( @roots ) {
@@ -77,6 +91,7 @@ for my $root ( @roots ) {
 
 	# get the NCBI root node object
 	my $rootnode = $dbt->get_taxon( '-taxonid' => $root );
+	$log->info("root node object: $rootnode");
 	
 	# start recursing
 	recurse($rootnode);
@@ -84,57 +99,51 @@ for my $root ( @roots ) {
 }
 
 sub recurse {
-	my $node = shift;
-	my $id   = $node->id;
-	$log->debug($id);
-	my @children = $dbt->each_Descendent($node);
+	my $node  = shift;
+	my $id    = $node->id;
+	my $name  = $node->scientific_name;
+	my $rank  = $node->rank;
+	my $count = 0;
+	$phylum = $id if $rank eq 'phylum';
+
+	# fetch immediate children
+	my @children = $dbt->each_Descendent($node);	
 	
 	# node is internal
 	if ( @children ) {
 		
 		# cache tip counts before further recursion
 		$before_treebase{$id} = $in_treebase;
-		$before_ncbi{$id} = $in_ncbi;
 		
 		# recurse further
 		recurse($_) for @children;
 		
-		# going to go by phyla
-		my $rank = $node->rank;
-		
-		# if count exceeds threshold, print result
-		if ( $rank eq 'phylum' ) {
-
-			# compute count of subtended NCBI tips
-			my $ncbi_tips = $in_ncbi - $before_ncbi{$id};
-		
-			# compute count of subtended TreeBASE tips
-			my $treebase_tips = $in_treebase - $before_treebase{$id};
+		# compute count of subtended TreeBASE tips
+		$count = $in_treebase - $before_treebase{$id};
 			
-			# print result
-			if ( $block{$id} ) {
-				$log->info("skipping unclassified id $id");
-			}
-			else {
-				my $name = $node->scientific_name;
-				print 
-					$id, "\t", 
-					$name, "\t", 
-					$rank, "\t", 
-					$treebase_tips, "\t", 
-					$ncbi_tips, "\n";
-			}
-		}
 	}
 	else {
+
+		# number of studies for focal tip
+		$count = $taxon{$id} || 0;
 		
-		# increment counter for every NCBI tip	
-		$in_ncbi++;
-		
-		# only increment counter for TreeBASE tip if seen
-		$in_treebase++ if $taxon{$id};
+		# counter for post-order diff computation
+		$in_treebase += $count;
+	}
+
+	# maybe ignore environmental samples, etc.
+	if ( $block{$id} ) {
+		$log->info("skipping blacklisted id $id");
+	}
+	else {
+        	print  
+                	$id, "\t",
+                	$name, "\t",
+                	$rank, "\t",
+                	$count, "\t",
+			scalar(@children), "\t",
+			$phylum, "\t",
+			++$pos, "\n";
+		$log->info("ID: $id, NAME: $name, COUNT: $count") if $rank eq 'phylum';
 	}
 }
-
-
-
