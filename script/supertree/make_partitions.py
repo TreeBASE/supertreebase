e356f36e9951ecb9701069b6989e21827b36a711
aiblaauw <aiblaauw@gmail.com>
2017-01-27 22:57:06 +0000

    adding first version of actual partitioning script

--- script/supertree/make_partitions.py
+++ script/supertree/make_partitions.py
@@ -5,13 +5,14 @@ Author: Astrid Blaauw
 Date: 16/01/2016
 
 Collects data as input for supertree analysis,
-using *.sdm files 
+using *.sdm files or *.tnt
 
 (Super)kingdom table input:
-	kingdom_ID species_count study_count study_ID(, study_ID)
+	kingdom_name species_count study_count study_ID(, study_ID)
 
 Usage:
     -i  Text file containing study_ID's linked to kingdom_tax_ID list
+    -a  (S/D), if data should be prepared for SDM or TNT analysis 
 '''
 
 import argparse
@@ -27,6 +28,8 @@ def main():
 	parser = argparse.ArgumentParser(description='Process commandline arguments')
 	parser.add_argument("-i", type=str,
     	                help="Text file containing study_ID's linked to kingdom_tax_ID list")
+	parser.add_argument("-a", type=str,
+    	                help="Analysis program where the data should be prepared for, SMD or TNT (S/T)")
 	args = parser.parse_args()
 	
 	#outname_log = args.o + args.i
@@ -34,6 +37,8 @@ def main():
 	#log_file = open(outname_log, "a")
 
 	kingdoms_file = open(args.i)
+
+	analysistype = args.a
 
 	datadir = "../../data/treebase/"
 
@@ -44,21 +49,43 @@ def main():
 		filecount = 0
 		out = ""
 
-		kingdom_file = open(datadir + "tb2dist_" + kingdom_name, "a")
+		#SDM partition
+		if analysistype == "S":
 
-		filelist = l.split()
-		for f in filelist:
-			f = f.replace(",", "").strip()
-			studyfiles = (datadir + f + "*.sdm")
-			datalist = glob.glob(studyfiles)
-			for tb in datalist:
-				tb_file = open(tb)
-				out += (tb_file.read())
-				tb_file.close()
-			filecount += len(datalist)
+			kingdom_file = open(datadir + "tb2dist_" + kingdom_name, "a")
 
-		kingdom_file.write(str(filecount) + "\n")
-		kingdom_file.write(out)
+			filelist = l.split()
+			for f in filelist:
+				f = f.replace(",", "").strip()
+				studyfiles = (datadir + f + "*.sdm")
+				datalist = glob.glob(studyfiles)
+				for tb in datalist:
+					tb_file = open(tb)
+					out += (tb_file.read())
+					tb_file.close()
+				filecount += len(datalist)
+
+			kingdom_file.write(str(filecount) + "\n")
+			kingdom_file.write(out)
+
+		#TNT partition
+		if analysistype == "T":
+
+			kingdom_file = open(datadir + "tntscript.run" + kingdom_name, "a")
+
+			filelist = l.split()
+			for f in filelist:
+				f = f.replace(",", "").strip()
+				studyfiles = (datadir + f + "*.tnt")
+				datalist = glob.glob(studyfiles)
+				for tb in datalist:
+					tb_file = open(tb)
+					out += (tb_file.read())
+					tb_file.close()
+				filecount += len(datalist)
+
+			kingdom_file.write(str(filecount) + "\n")
+			kingdom_file.write(out)
 
 	kingdoms_file.close()
 
