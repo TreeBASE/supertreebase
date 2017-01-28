ecda2873d1a3030057f586919f01e621592c70b3
aiblaauw <aiblaauw@gmail.com>
2017-01-28 13:45:07 +0000

    adding expantions, but there seem to be some problems with the nchar.txt file

--- script/supertree/make_partitions.py
+++ script/supertree/make_partitions.py
@@ -13,6 +13,7 @@ using *.sdm files or *.tnt
 Usage:
     -i  Text file containing study_ID's linked to kingdom_tax_ID list
     -a  (S/D), if data should be prepared for SDM or TNT analysis 
+    -n  Tree-block ID's linked to number of characters 
 '''
 
 import argparse
@@ -30,6 +31,8 @@ def main():
     	                help="Text file containing study_ID's linked to kingdom_tax_ID list")
 	parser.add_argument("-a", type=str,
     	                help="Analysis program where the data should be prepared for, SMD or TNT (S/T)")
+	parser.add_argument("-n", type=str,
+    	                help="Table containing Tb_ID's linked to number of character found in tree")
 	args = parser.parse_args()
 	
 	#outname_log = args.o + args.i
@@ -37,6 +40,17 @@ def main():
 	#log_file = open(outname_log, "a")
 
 	kingdoms_file = open(args.i)
+	
+	nchar_file = open(args.n)
+	nchar_dict = dict()
+
+	print("reading nchar.txt ...")
+	for l in nchar_file:
+		l = l.strip()
+		if l.count("Tb") == 1 and len(l.split()) == 2:
+			tb_id = l.split()[0]
+			nchar = l.split()[1]
+			nchar_dict[tb_id] = nchar
 
 	analysistype = args.a
 
@@ -45,6 +59,7 @@ def main():
 	for l in kingdoms_file:
 		l = l.strip()
 		kingdom_name = l.split()[0]
+		print("processing " + kingdom_name + " ...")
 
 		filecount = 0
 		out = ""
@@ -72,6 +87,8 @@ def main():
 		if analysistype == "T":
 
 			kingdom_file = open(datadir + "tntscript.run" + kingdom_name, "a")
+			charcount = 0
+			specieslist = list()
 
 			filelist = l.split()
 			for f in filelist:
@@ -79,14 +96,38 @@ def main():
 				studyfiles = (datadir + f + "*.tnt")
 				datalist = glob.glob(studyfiles)
 				for tb in datalist:
+					print("processing " + tb + " ...")
+
 					tb_file = open(tb)
-					out += (tb_file.read())
+					out += ("& [ num ] @@ {} data ;\n".format(tb) )
+
+					for l in tb_file:
+						l = l.strip()
+						components = l.split()
+						if len(components) != 1 and components[0] != "Label":
+							tb_name = tb.split(".")[-2]							
+							if tb_name in nchar_dict.keys():
+								charcount += int(nchar_dict[tb_name])
+							tax = components[0]
+							if tax not in specieslist:
+								specieslist.append(tax)
+
 					tb_file.close()
 				filecount += len(datalist)
+			
+			species_count = len(specieslist)
 
-			kingdom_file.write(str(filecount) + "\n")
+			intro = """
+macro=;
+/* execute me be starting up TNT and then type 'proc <scriptname> ;' */
+/* note that you will need to increase memory to at least 50Gb! */
+/* memory is increased in megabytes, e.g. using 'mxram 50000 ;' */
+nstates 2;
+xread
+{} {} \n""".format(charcount, species_count) 
+			kingdom_file.write(intro)
 			kingdom_file.write(out)
-
+			kingdom_file.write("proc/;\n")
 	kingdoms_file.close()
 
 if __name__ == "__main__":
